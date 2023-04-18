2dfa3fd1e40686fadb58a4bbcbb246cfe3d2f028
Sean McPherson <mail@seanmcp.com>
2023-04-17 21:28:23 -0400

    build multiple tables

--- build.mjs
+++ build.mjs
@@ -1,48 +1,50 @@
 import fs from "fs";
 
-const SIZE = 10;
+const tableHTML = fs.readFileSync(".table.html", "utf8");
+const indexHTML = fs.readFileSync(".index.html", "utf8");
 
-let inputs = ``;
-const rows = {};
-const columns = {};
+function buildPage(SIZE) {
+  let inputs = ``;
+  const rows = {};
+  const columns = {};
 
-const ids = [];
+  const ids = [];
 
-for (let x = 0; x <= SIZE; x++) {
-  for (let y = 0; y <= SIZE; y++) {
-    if (x === 0 && y === 0) {
-      inputs += "<span>✖</span>";
-    } else if (x === 0) {
-      inputs += `<span>${y}</span>`;
-    } else if (y === 0) {
-      inputs += `<span>${x}</span>`;
-    } else {
-      const id = `${x}x${y}`;
+  for (let x = 0; x <= SIZE; x++) {
+    for (let y = 0; y <= SIZE; y++) {
+      if (x === 0 && y === 0) {
+        inputs += "<span>✖</span>";
+      } else if (x === 0) {
+        inputs += `<span>${y}</span>`;
+      } else if (y === 0) {
+        inputs += `<span>${x}</span>`;
+      } else {
+        const id = `${x}x${y}`;
 
-      if (!rows[x]) rows[x] = [];
-      rows[x].push(id);
+        if (!rows[x]) rows[x] = [];
+        rows[x].push(id);
 
-      if (!columns[y]) columns[y] = [];
-      columns[y].push(id);
+        if (!columns[y]) columns[y] = [];
+        columns[y].push(id);
 
-      ids.push(id);
+        ids.push(id);
 
-      const product = x * y;
-      inputs += `<input name="${id}" pattern="^${product}$" required size="${
-        String(product).length
-      }" />\n`;
+        const product = x * y;
+        inputs += `<input name="${id}" pattern="^${product}$" required size="${
+          String(product).length
+        }" />\n`;
+      }
     }
   }
-}
 
-let selectors = ``;
-const styles = `background-color: var(--complete, gold);`;
+  let selectors = ``;
+  const styles = `background-color: var(--complete, gold);`;
 
-const winner = `body:is(.DEVMODE, ${ids
-  .map((id) => `:has(input[name="${id}"]:valid)`)
-  .join("")})`;
+  const winner = `body:is(.DEVMODE, ${ids
+    .map((id) => `:has(input[name="${id}"]:valid)`)
+    .join("")})`;
 
-selectors += `${winner} {
+  selectors += `${winner} {
     background-color: var(--winner);
 }
 
@@ -55,35 +57,49 @@ ${winner} .grid {
 }
 `;
 
-Object.entries(rows).forEach(([key, value]) => {
-  let selector = `.grid`;
-  value.forEach((id) => {
-    selector += `:has(input[name="${id}"]:valid)`;
+  Object.entries(rows).forEach(([key, value]) => {
+    let selector = `.grid`;
+    value.forEach((id) => {
+      selector += `:has(input[name="${id}"]:valid)`;
+    });
+    selector += ` [name^="${key}x"] { ${styles} }\n`;
+    selectors += selector;
   });
-  selector += ` [name^="${key}x"] { ${styles} }\n`;
-  selectors += selector;
-});
 
-Object.entries(columns).forEach(([key, value]) => {
-  let selector = `.grid`;
-  value.forEach((id) => {
-    selector += `:has(input[name="${id}"]:valid)`;
+  Object.entries(columns).forEach(([key, value]) => {
+    let selector = `.grid`;
+    value.forEach((id) => {
+      selector += `:has(input[name="${id}"]:valid)`;
+    });
+    selector += ` [name$="x${key}"] { ${styles} }\n`;
+    selectors += selector;
   });
-  selector += ` [name$="x${key}"] { ${styles} }\n`;
-  selectors += selector;
-});
 
-fs.writeFileSync(
-  "selectors.css",
-  "/** GENERATED FILE: DO NOT EDIT DIRECTLY */\n" + selectors
-);
+  fs.writeFileSync(
+    `${SIZE}.css`,
+    "/** GENERATED FILE: DO NOT EDIT DIRECTLY */\n" + selectors
+  );
 
-const gridSize = SIZE + 1;
+  const gridSize = SIZE + 1;
 
-const template = fs.readFileSync(".template.html", "utf8");
-const html = template
-  .replace("<!-- INPUTS -->", inputs)
-  .replace("/** GRID_STYLE */", `--grid-size: ${gridSize};`);
+  const html = tableHTML
+    .replace("%INPUTS%", inputs)
+    .replace("%GRID_STYLE%", `--grid-size: ${gridSize};`)
+    .replaceAll("%SIZE%", SIZE);
+  fs.writeFileSync(
+    `${SIZE}.html`,
+    "<!-- GENERATED FILE: DO NOT EDIT DIRECTLY -->\n" + html
+  );
+}
+
+let links = "";
+
+for (let i = 1; i < 13; i++) {
+  links += `<a href="${i}.html">${i} by ${i} table</a>`;
+  buildPage(i);
+}
+
+const html = indexHTML.replace("%LINKS%", links);
 fs.writeFileSync(
   "index.html",
   "<!-- GENERATED FILE: DO NOT EDIT DIRECTLY -->\n" + html
